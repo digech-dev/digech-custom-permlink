e5a8b01e378ec591ed7c99671be43d4ddbf71098
digech <github@digech.com>
2026-08-17 20:10:42 +0900

    WooCommerce product ID permalink
    Added custom permalink structure for WooCommerce products and implemented rewrite rules for product URLs.

--- digech-custom-permlink.php
+++ digech-custom-permlink.php
@@ -10,3 +10,44 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+/**
+ * WooCommerce product permalink
+ * /product/12345/
+ */
+add_filter( 'post_type_link', function ( $url, $post, $leavename ) {
+
+	if ( 'product' !== $post->post_type ) {
+		return $url;
+	}
+
+	return home_url( '/product/' . $post->ID . '/' );
+
+}, 10, 3 );
+
+/**
+ * Rewrite /product/{ID}/ to WooCommerce product.
+ */
+add_action( 'init', function () {
+
+	add_rewrite_rule(
+		'^product/([0-9]+)/?$',
+		'index.php?post_type=product&p=$matches[1]',
+		'top'
+	);
+
+} );
+
+/**
+ * Flush rewrite rules when plugin is activated.
+ */
+register_activation_hook( __FILE__, function () {
+	flush_rewrite_rules();
+} );
+
+/**
+ * Flush rewrite rules when plugin is deactivated.
+ */
+register_deactivation_hook( __FILE__, function () {
+	flush_rewrite_rules();
+} );
