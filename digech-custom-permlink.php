4fbbc07bb96a21f2af1e5de3421e098ea5cfcc2f
digech <github@digech.com>
2026-08-17 20:08:34 +0900

    Create Digech Custom Permalink plugin file

---
+++ digech-custom-permlink.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Digech Custom Permalink
+ * Description: Custom permalink management for WordPress and WooCommerce.
+ * Version: 1.0.0
+ * Author: Digech
+ * License: Proprietary
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
