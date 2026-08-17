732d02bf3619bfdf427ebe5a98772e0125dc3c6a
digech <github@digech.com>
2026-08-17 22:29:39 +0900

    Bump version to 1.0.4 in plugin header

--- digech-custom-permlink.php
+++ digech-custom-permlink.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Digech Custom Permalink
  * Description: Custom permalink management for WordPress and WooCommerce.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Digech
  * License: Proprietary
  */
