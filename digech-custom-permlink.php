b2fab5c303d671a5e9c05d16e9a598ebc9a96231
digech <github@digech.com>
2026-08-17 21:31:53 +0900

    Update GitHub repository URL in update checker

--- digech-custom-permlink.php
+++ digech-custom-permlink.php
@@ -95,7 +95,7 @@ require_once __DIR__ . '/plugin-update-checker-master/plugin-update-checker.php'
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $update_checker = PucFactory::buildUpdateChecker(
-	'https://github.com/YOUR-GITHUB-USERNAME/digech-custom-permlink/',
+	'https://github.com/digech-dev/digech-custom-permlink/',
 	__FILE__,
 	'digech-custom-permlink'
 );
